f5b3e0f9baee7b8014f9258cce6b721e6c90ab9e
One-X-Two <rusllann97@gmail.com>
2020-11-11 08:39:28 +0500

    Добавил в класс Житель логику проверки времени путешествия. Добавил пополнение HP при возвращении жителя. Начал писать функцию сбора(скорее обновления) данных о жителях убежища

--- Vault13Server/Dweller.cs
+++ Vault13Server/Dweller.cs
@@ -12,7 +12,9 @@ namespace Vault13Server
         const UInt16 maxHealth = 100;
 
         UInt16 healthPoints = maxHealth;
+        DateTime timeOfAdventureBegin = DateTime.MinValue;
 
+        Status personalStatus = Status.IN_VAULT;
         public enum Status
         {
             IN_VAULT,
@@ -57,9 +59,41 @@ namespace Vault13Server
             healthPoints = maxHealth;
         }
 
-        public Status PersonalStatus { get; set; }
+        public Status PersonalStatus 
+        { 
+            get
+            {
+                return personalStatus;
+            }
+            set
+            {
+                if (value == Status.IN_WASTELAND)
+                {
+                    //Если отправляем жителя в пустоши - устанавливаем время начала путешествия.
+                    TimeOfAdventureBegin = DateTime.Now;
+                }
+                else if(value == Status.IN_VAULT)
+                {
+                    //Если житель вернулся в убежище - его здоровье пополняется полностью
+                    FillHealth();
+                }
+            }
+        }
 
+        public UInt16 WastelandResearchTimeSec
+        {
+            set; get;
+        }
 
+        public bool IsReturnedFromWasteland()
+        {
+            //long timePassedMs = (DateTime.UtcNow.Ticks- TimeOfAdventureBegin.Ticks)/ TimeSpan.TicksPerMillisecond; // Должно работать
+            long timePassedMs = DateTime.UtcNow.ToFileTimeUtc() - TimeOfAdventureBegin.ToFileTimeUtc();
+            if (timePassedMs >= WastelandResearchTimeSec)
+                return true;
+
+            return false;
+        }
         public Dweller(string name)
         {
             Name = name;
@@ -70,6 +104,12 @@ namespace Vault13Server
         {
             Name = "Stranger" + NewDwellerNameCount.ToString();
             NewDwellerNameCount++;
+        }
+
+        public DateTime TimeOfAdventureBegin
+        {
+            set;
+            get;
         }
     }
 }
--- Vault13Server/Vault.cs
+++ Vault13Server/Vault.cs
@@ -10,6 +10,26 @@ namespace Vault13Server
     {
         List<Dweller> dwellersList;
 
+        public UInt16 UpdateInfoPeriodSec
+        {
+            set;get;
+        }
+        //1 реальная секунда - 1 час игрового времени
+        public void NotifyVault()
+        {
+            //стоит запускать в другой таске
+            foreach (var dweller in dwellersList)
+            {
+                if (dweller.PersonalStatus == Dweller.Status.IN_WASTELAND)
+                {
+                    dweller.PersonalStatus = Dweller.Status.IN_WASTELAND;
+                    if(DateTime.Now == dweller.TimeOfAdventureBegin)
+
+                    //Запустить в другой таске таймер на timeInHours;
+                    return true;
+                }
+            }
+        }
         public bool SendDwellerToWasteland(string name, UInt16 timeInHours)
         {
             foreach (var dweller in dwellersList)
